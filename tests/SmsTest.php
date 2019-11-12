87f6da2ae02af4c496b465865f7e5f65df0fd43f
Admin <wuhong@yeah.net>
2019-11-12 16:04:55 +0800

    1.4

--- tests/SmsTest.php
+++ tests/SmsTest.php
@@ -16,20 +16,20 @@ class SmsTest extends TestCase
     {
         $config = [
             'url' => 'http://risk.4255.cn/',
-            'token' => 'b08d2c0f36e6d39cfa2b76f9b9380615c10eb154d606ecafc7d0eff19a44cbc1',
-            'secret_key' => 'fb2be0d575174c0ab3d5ea2912bcda31',
+            'token' => 'b08d2c0f36e6d39cfa2b76f9b9380615c10eb154d606ecafc7d0eff19a44cbc2342341',
+            'secret_key' => 'fb2be0d575174c0ab3d5ea2923423423412bcda31',
         ];
 
 
         $risk = new Risk();
         $risk->setConfig($config);
         //短信
-//        $result = $risk->MessageSend('13983087661', 33, ["code" => 1111]);
+//        $result = $risk->MessageSend('13983087666', 33, ["code" => 1111]);
 //银行卡四要素
-        $result = $risk->CertificationBankFour('吴红', '13983087661',"512322198112204917","6221340013257480");
-        //手机三要素
-//        $result = $risk->CertificationMobileThree('吴红', '13983087661', "512322198112204917");
-        return var_dump($result);
+//        $result = $risk->CertificationBankFour('吴红', '13983087666',"512322198112202344917","6221342342340013257480");
+        //手机三要素（未验证）
+//        $result = $risk->CertificationMobileThree('吴红', '13983087666', "512322198112204917");
+//        return var_dump($result);
     }
 
 }
